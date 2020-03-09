bf28a9259a2beabf965b54bdb152a251203ab694
Tomas <tomas@tomas.com>
2020-03-09 11:18:56 +0100

    Clase TSLFunciona

---
+++ sumador/src/sumador/TSLFunciona.java
@@ -0,0 +1,16 @@
+package sumador;
+
+public class TSLFunciona {
+
+	public static void main(String[] args) {
+		System.out.println("Introducimos un 5:");
+		ASumar suma1 = new ASumar("5");
+		System.out.println(suma1.mostrar());
+		System.out.println("Introducimos un -4:");
+		ASumar suma2 = new ASumar("5");
+		System.out.println(suma1.mostrar());
+		System.out.println("Introducimos un 25:");
+		ASumar suma3 = new ASumar("5");
+		System.out.println(suma1.mostrar());
+	}
+}
